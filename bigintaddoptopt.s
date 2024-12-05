d5e6fa1dca1d036d5cf12ca33169298111d6e27a
anish <anish@dynamic-oit-ip4-wifirestricted03-10-49-10-112.princeton.edu>
2024-12-04 19:24:00 -0500

    hsjhdhj

--- bigintaddoptopt.s
+++ bigintaddoptopt.s
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------
 // bigintaddoptopt.s
-// Author: anish
+// Author: Anish K 
 //-----------------------------------------------------------------------
 
 // Defining constants
@@ -16,7 +16,6 @@
 .equ    ADDITION_STACK_SIZE, 48  // Reduced stack size after optimizations
 
 // Assign meaningful register names to variables using .req directives
-ULCARRY         .req x22    // ulCarry (handled by carry flag, no longer needed)
 ULSUM           .req x23    // ulSum
 LINDEX          .req x24    // lIndex
 LSUMLENGTH      .req x25    // lSumLength
@@ -57,8 +56,8 @@ BigInt_add:
     mov     OSUM, x2                          // oSum = x2
 
     // Determine the larger length: lSumLength = max(oAddend1->lLength, oAddend2->lLength)
-    ldr     x19, [OADDEND1, LENGTH_OFFSET]     // load oAddend1->lLength into LLENGTH1 (x19)
-    ldr     x20, [OADDEND2, LENGTH_OFFSET]     // load oAddend2->lLength into LLENGTH2 (x20)
+    ldr     x19, [OADDEND1, LENGTH_OFFSET]     // load oAddend1->lLength into x19
+    ldr     x20, [OADDEND2, LENGTH_OFFSET]     // load oAddend2->lLength into x20
     cmp     x19, x20                          // compare lLength1 and lLength2
     bgt     set_lsumlength1                    // if lLength1 > lLength2, set lSumLength = lLength1
     mov     LSUMLENGTH, x20                    // else, lSumLength = lLength2
@@ -70,12 +69,12 @@ set_lsumlength1:
 after_lsumlength:
 
     // Check if oSum->lLength <= lSumLength
-    ldr     x21, [OSUM, LENGTH_OFFSET]         // load oSum->lLength
+    ldr     x21, [OSUM, LENGTH_OFFSET]         // load oSum->lLength into x21
     cmp     x21, LSUMLENGTH
     ble     skip_clear_digits                   // if oSum->lLength <= lSumLength, skip memset
 
     // Perform memset(oSum->aulDigits, 0, MAX_DIGITS_COUNT * sizeof(unsigned long))
-    add     x0, OSUM, DIGITS_OFFSET            // pointer to oSum->aulDigits
+    add     x16, OSUM, DIGITS_OFFSET            // x16 = OSUM + DIGITS_OFFSET (base address)
     mov     w1, 0                              // value to set
     mov     x2, MAX_DIGITS_COUNT               // size
     lsl     x2, x2, #3                         // multiply by 8 (sizeof(unsigned long))
@@ -95,19 +94,25 @@ sumLoop:
     bge     handle_carry                       // if lIndex >= lSumLength, handle carry
 
     // Load oAddend1->aulDigits[lIndex]
-    ldr     x0, [OADDEND1, DIGITS_OFFSET, LINDEX, lsl #3] // load oAddend1->aulDigits[lIndex] into x0
+    ldr     x16, [OADDEND1, DIGITS_OFFSET]     // Load base address of oAddend1->aulDigits into x16
+    add     x16, x16, LINDEX, lsl #3           // x16 = x16 + (lIndex << 3)
+    ldr     x0, [x16]                           // Load oAddend1->aulDigits[lIndex] into x0
 
     // Add oAddend1->aulDigits[lIndex] to ulSum with carry
     adcs    x23, xzr, x0                        // ulSum = ulSum + oAddend1->aulDigits[lIndex] + carry
 
     // Load oAddend2->aulDigits[lIndex]
-    ldr     x1, [OADDEND2, DIGITS_OFFSET, LINDEX, lsl #3] // load oAddend2->aulDigits[lIndex] into x1
+    ldr     x17, [OADDEND2, DIGITS_OFFSET]     // Load base address of oAddend2->aulDigits into x17
+    add     x17, x17, LINDEX, lsl #3           // x17 = x17 + (lIndex << 3)
+    ldr     x1, [x17]                           // Load oAddend2->aulDigits[lIndex] into x1
 
     // Add oAddend2->aulDigits[lIndex] to ulSum with carry
     adcs    x23, xzr, x1                        // ulSum = ulSum + oAddend2->aulDigits[lIndex] + carry
 
     // Store ulSum into oSum->aulDigits[lIndex]
-    str     x23, [OSUM, DIGITS_OFFSET, LINDEX, lsl #3] // store ulSum into oSum->aulDigits[lIndex]
+    ldr     x16, [OSUM, DIGITS_OFFSET]         // Load base address of oSum->aulDigits into x16
+    add     x16, x16, LINDEX, lsl #3           // x16 = x16 + (lIndex << 3)
+    str     x23, [x16]                           // Store ulSum into oSum->aulDigits[lIndex]
 
     // Increment lIndex
     add     LINDEX, LINDEX, 1
@@ -124,17 +129,17 @@ handle_carry:
     beq     overflow_detected                   // if equal, overflow occurred
 
     // Set oSum->aulDigits[lSumLength] = 1
-    add     x0, OSUM, DIGITS_OFFSET            // pointer to oSum->aulDigits
-    add     x0, x0, LSUMLENGTH, lsl #3         // address of oSum->aulDigits[lSumLength]
+    add     x16, OSUM, DIGITS_OFFSET            // Load base address of oSum->aulDigits into x16
+    add     x16, x16, LSUMLENGTH, lsl #3        // x16 = x16 + (lSumLength << 3)
     mov     x1, 1
-    str     x1, [x0]                            // set the carry digit
+    str     x1, [x16]                            // Set the carry digit to 1
 
     // Increment lSumLength
     add     LSUMLENGTH, LSUMLENGTH, 1
 
 finalize_sum_length:
     // Set oSum->lLength = lSumLength
-    str     LSUMLENGTH, [OSUM, LENGTH_OFFSET]   // store lSumLength into oSum->lLength
+    str     LSUMLENGTH, [OSUM, LENGTH_OFFSET]    // Store lSumLength into oSum->lLength
 
     // Epilogue: restore stack frame and return
     mov     w0, TRUE_VAL                        // return TRUE_VAL
